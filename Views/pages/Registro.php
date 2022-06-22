f4ef79e46300d0a0407336f3de4844a45ab03f42
ArianaST <marinestrellita@hotmail.com>
2022-06-22 13:40:41 -0500

    RolesComboBox

--- Views/pages/Registro.php
+++ Views/pages/Registro.php
@@ -24,21 +24,7 @@
     </div>
 
     <!-- Login Form -->
-    <form method="post" id="formularioRegistro" name="formularioRegistro" action="../../Actions/Register/registrar.php">
-    <div class="dropdown show">
-  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Roles
-  </a>
-
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <a class="dropdown-item" href="#">Coordinador de tutores</a>
-    <a class="dropdown-item" href="#">Orientador educativo</a>
-    <a class="dropdown-item" href="#">Administrador</a>
-    <a class="dropdown-item" href="#">Tutor académico</a>
-    <a class="dropdown-item" href="#">Alumno</a>
-    <a class="dropdown-item" href="#">Padres de familia</a>
-  </div>
-</div> 
+     <form method="post" id="formularioRegistro" name="formularioRegistro" action="../../Actions/Register/registrar.php">
       <input type="text" id="codigoUsuario" class="fadeIn second" name="codigoUsuario" placeholder="Nombre">
       <input type="text" id="direccionUsuario" class="fadeIn second" name="direccionUsuario" placeholder="Apellidos">
       <input type="text" id="nss" class="fadeIn second" name="nss" placeholder="E-Mail">
@@ -46,7 +32,19 @@
       <input type="text" id="curp" class="fadeIn second" name="curp" placeholder="Contraseña">
       <input type="text" id="alergia" class="fadeIn second" name="alergia" placeholder="Status">
       <input type="text" id="alergia" class="fadeIn second" name="alergia" placeholder="Status">
-      <input type="submit" class="fadeIn fourth" value="Registrarse">
+    <!---Agregando ComboBox -->
+
+    <div class="dropdown">
+  <button class="dropbtn">   Roles</button>
+  <div class="dropdown-content">
+    <a href="#">Coordinador de tutores</a>
+    <a href="#">Orientador educativo</a>
+    <a href="#">Administrador</a>
+    <a href="#">Tutor académico</a>
+    <a href="#">Alumno</a>
+    <a href="#">Padres de familia</a>
+  </div>
+  <input type="submit" class="fadeIn fourth" value="Registrarse">
     </form>
 
     <!-- Remind Passowrd -->
