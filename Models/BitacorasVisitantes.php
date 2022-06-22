48d0cf1d8310fb63b5752bdee9b801ed55d5cbeb
Jesalex3 <jesusalexandro099@gmail.com>
2022-06-22 10:10:39 -0700

    Model BitacorasVisitantes

---
+++ Models/BitacorasVisitantes.php
@@ -0,0 +1,20 @@
+<?php
+class 
+{
+    private $id;
+    private $HoraEntrada;
+    private $HoraSalida;
+    private $Observacion;
+    private $Visitantes_id;
+
+    public function __GET($k)
+    {
+        return $this->$k;
+    }
+
+    public function __SET($k,$v)
+    {
+        return $this->$k = $v;
+    }
+}
+?>
