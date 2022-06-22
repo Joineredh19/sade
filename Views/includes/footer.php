52bf75967c64d61cf2e150fe5aa4729da4998c53
iAmBeeGees <walk414126@gmail.com>
2022-06-22 10:58:53 -0500

    Estadísticas Insertadas

--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -33,7 +33,7 @@
   <script src="../Resource/js/plugins/bootstrap-notify.js"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="../Resource/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="../Resource/demo/demo.js"></script>
+  <script src="../Resource/css/demo/demo.js"></script>
   <script>
     $(document).ready(function() {
       // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
