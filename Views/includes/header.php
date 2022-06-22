109497f286eae9327b5b1f1c36b2af6c9fe41593
iAmBeeGees <walk414126@gmail.com>
2022-06-22 10:47:32 -0500

    Dashboard Admin

--- Views/includes/header.php
+++ Views/includes/header.php
@@ -5,16 +5,26 @@ if(!isset($_SESSION["Usuario"])){
 }*/
 ?>
 
+
+
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-
-    <title>SADE</title>
-
-    <link href="../../Resource/css/login.css"  rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="" href="../assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Paper Dashboard 2 by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="../Resource/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="../Resource/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="../Resource/css/demo/demo.css" rel="stylesheet" />
 </head>
-
-<body>
