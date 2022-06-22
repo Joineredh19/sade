83f17116b81cf3fa6a01af8b10aadd9ad7fbd7c4
iAmBeeGees <walk414126@gmail.com>
2022-06-22 12:24:08 -0500

    Roles Combobox

--- Views/pages/Registro.php
+++ Views/pages/Registro.php
@@ -25,13 +25,27 @@
 
     <!-- Login Form -->
     <form method="post" id="formularioRegistro" name="formularioRegistro" action="../../Actions/Register/registrar.php">
-      <input type="text" id="codigoUsuario" class="fadeIn second" name="codigoUsuario" placeholder="Ingresa tú codigo">
-      <input type="text" id="direccionUsuario" class="fadeIn second" name="direccionUsuario" placeholder="Ingresa tú dirección">
-      <input type="text" id="nss" class="fadeIn second" name="nss" placeholder="Ingresa tú número de seguro social">
-      <input type="text" id="rfc" class="fadeIn second" name="rfc" placeholder="Ingresa tú RFC (13 digitos)">
-      <input type="text" id="curp" class="fadeIn second" name="curp" placeholder="Ingresa tú CURP (18 digitos)">
-      <input type="text" id="tipoSangre" class="fadeIn second" name="tipoSangre" placeholder="Ingresa tú tipo de sangre">
-      <input type="text" id="alergia" class="fadeIn second" name="alergia" placeholder="Ingresa si tienes alguna alergía">
+    <div class="dropdown show">
+  <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Roles
+  </a>
+
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <a class="dropdown-item" href="#">Coordinador de tutores</a>
+    <a class="dropdown-item" href="#">Orientador educativo</a>
+    <a class="dropdown-item" href="#">Administrador</a>
+    <a class="dropdown-item" href="#">Tutor académico</a>
+    <a class="dropdown-item" href="#">Alumno</a>
+    <a class="dropdown-item" href="#">Padres de familia</a>
+  </div>
+</div> 
+      <input type="text" id="codigoUsuario" class="fadeIn second" name="codigoUsuario" placeholder="Nombre">
+      <input type="text" id="direccionUsuario" class="fadeIn second" name="direccionUsuario" placeholder="Apellidos">
+      <input type="text" id="nss" class="fadeIn second" name="nss" placeholder="E-Mail">
+      <input type="text" id="rfc" class="fadeIn second" name="rfc" placeholder="Teléfono">
+      <input type="text" id="curp" class="fadeIn second" name="curp" placeholder="Contraseña">
+      <input type="text" id="alergia" class="fadeIn second" name="alergia" placeholder="Status">
+      <input type="text" id="alergia" class="fadeIn second" name="alergia" placeholder="Status">
       <input type="submit" class="fadeIn fourth" value="Registrarse">
     </form>
 
