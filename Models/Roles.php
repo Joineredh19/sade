a191218f897b55496a88e8e97436132f460e2eed
Jesalex3 <jesusalexandro099@gmail.com>
2022-06-22 09:24:02 -0700

    Model Roles

---
+++ Models/Roles.php
@@ -0,0 +1,16 @@
+<?php
+class Roles
+{
+    private $id;
+    private $rol;
+    public function __GET($k)
+    {
+        return $this->$k;
+    }
+
+    public function __SET($k,$v)
+    {
+        return $this->$k = $v;
+    }
+}
+?>
