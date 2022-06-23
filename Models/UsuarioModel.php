23dd43b6ba3ebc39588856f46de9a6d200f227d3
Joiner2003 <Joineredh@gmail.com>
2022-06-22 23:09:15 -0500

    Modelo Usuario

--- Models/UsuarioModel.php
+++ Models/UsuarioModel.php
@@ -19,7 +19,7 @@ require_once('../../Models/Conexion/Conexion.php');
 		}
 		
 
-		public function Ver($IdUsuario)
+	/*	public function Ver($IdUsuario)
 		{
 			try{
 				$stm = $this->pdo->prepare("SELECT * FROM Usuario WHERE IdUsuario = ?");
@@ -137,7 +137,23 @@ require_once('../../Models/Conexion/Conexion.php');
 			} catch (Exception $e) {
 				die($e->getMessage()." -> Ota_UsuarioModel->Eliminar()");
 			}
+		}*/
+
+		public function Agregar($Nombres,$Apellidos,$Email,$Telefono,$Status,$RolesId,$FechaAlta,$Contraseña)
+		{
+			try {
+				$sql = ("INSERT INTO tusuarios  (Fecha1, Fecha2, Pte_NumDoc, Pte_TipoDoc, Pte_FechaNac, Pte_Edad, Pte_Ap1, Pte_Ap2, PteNom1, Pte_Nom2, Aco_Nombres, Aco_Apellidos, Aco_Documento, Aco_Perentezco, Sv_Origen, Sv_Origen1, Sv_Origen2, Sv_Origen3, Sv_Llegada, Sv_Llegada1, Sv_Llegada2, Sv_Llegada3, Sv_Salida, Sv_Salida1, Sv_Salida2, Sv_Salida3, Sv_Complejidad, Sv_TipoServicio, Sv_ExamenSolicitado, Sv_Firma_Pte2, En_Firma2, Sv_Firma_Entrega2, Ef_Ta, Ef_Fr, Ef_Temp, Ef_Glasgow, Ef_Dx1, Ef_Dx2, Ef_HallazgoPos1, Ef_Antecedentes1, Ef_Gin1, Ef_Gin2, Ef_Gin3, Ef_Gin4, Ef_Gin5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+				$stm = $this->pdo->prepare($sql)->execute(array($Nombres,$Apellidos,$Email,$Telefono,$Status,$RolesId,$FechaAlta,$Contraseña));
+				if($stm){
+					return true;
+				} else {
+					return false;
+				}
+			} catch (Exception $e) {
+				die($e->getMessage()." ->UsuarioModel->Agregar()");
+			}
 		}
+		
 
 
 
