ce418be7b36be166642b5ac9f46030bffa68fb3d
ArianaST <marinestrellita@hotmail.com>
2022-06-23 11:15:10 -0500

    registroTerminado

--- Views/pages/Registro.php
+++ Views/pages/Registro.php
@@ -25,27 +25,11 @@
     </div>
 
     <!-- Login Form -->
-
      <form method="post" id="formularioRegistro" name="formularioRegistro" action="../../Actions/Register/registrar.php">
      <select name="Roles" id="Roles" class="form-control" value="">
-
-     <form method="post" action="../../Actions/register/registrar.php">
-     <select name="Roles" id="Roles" class="fadeIn second" value="">
-      </select>
-<<<<<<< HEAD
-      <input type="text" id="nombreUsuario" class="fadeIn second" name="nombreUsuario" placeholder="Nombre">
-      <input type="text" id="apellidosUsuario" class="fadeIn second" name="apellidosUsuario" placeholder="Apellidos">
-      <input type="text" id="email" class="fadeIn second" name="email" placeholder="E-Mail">
-      <input type="text" id="telefono" class="fadeIn second" name="telefono" placeholder="Teléfono">
-      <input type="text" id="contrasena" class="fadeIn second" name="contrasena" placeholder="Contraseña">
-    <!---Agregando ComboBox -->
-   <br>
-  <!--  <div class="dropdown">
-  <button class="dropbtn">   Roles</button>
-=======
       <br>
       <input type="form" id="nombreUsuario" class="form-control" name="nombreUsuario" placeholder="Nombre">
-      <br>
+      <br> 
       <input type="form" id="apellidosUsuario" class="form-control" name="apellidosUsuario" placeholder="Apellidos">
       <br>
       <input type="form" id="email" class="form-control" name="email" placeholder="E-Mail">
@@ -53,20 +37,6 @@
       <input type="telefono" id="telefono" class="form-control" name="telefono" placeholder="Teléfono">
       <br>
       <input type="password"  class="form-control" id="contrasena"  name="contrasena" placeholder="Contraseña">
-        <!---Agregando ComboBox -->
-
-<!--  <button class="dropbtn">   Roles</button>
->>>>>>> 3804faae49032090286552a9f86c161a89ca4e89
-  <br>
-  <div class="dropdown-content">
-    <a href="#">Coordinador de tutores</a>
-    <a href="#">Orientador educativo</a>
-    <a href="#">Administrador</a>
-    <a href="#">Tutor académico</a>
-    <a href="#">Alumno</a>
-    <a href="#">Padres de familia</a>
-    <br>
-  </div>-->
   <br>
   <input type="submit" class="fadeIn fourth" id="btnRegistrarse" value="Registrarse">
     </form>
