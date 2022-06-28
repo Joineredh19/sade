d830630abe7bb4e6eae1437944c9760926bbf6a8
ArianaST <marinestrellita@hotmail.com>
2022-06-28 17:03:27 -0500

    Añadiendologo

--- Views/pages/Registro.php
+++ Views/pages/Registro.php
@@ -3,7 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-
+    <link rel="apple-touch-icon" sizes="76x76" href="../Resource/img/UDG- VectorR.png">
+ <link rel="icon" type="image/png" href="../Resource/img/UDG- VectorR.png">
     <title>SADE</title>
 
     <link href="../Resource/css/login.css"  rel="stylesheet" type="text/css">
