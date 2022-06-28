8bfdf2d86329b4f732cd9ceb1677b32c5f610413
Adbel79 <adbel79@gmail.com>
2022-06-28 16:08:26 -0500

    agregando parte de genero back-end

---
+++ Actions/Genero/Genero.php
@@ -0,0 +1,31 @@
+<?php
+function Conexion(){
+
+   // $serverName = "127.0.0.1";
+  //  $connectionOptions = array("Database"=>"sade22","Uid"=>"root", "PWD"=>"", "CharacterSet"=>"UTF-8");
+    $conn = mysqli_connect("bzya6emoafjbqjkolhiy-mysql.services.clever-cloud.com",
+     "ud5ggwh0iuxsr8up", "SttfR9Di2pih75lEFuum", "bzya6emoafjbqjkolhiy");
+   //  mysqli_connect($serverName, $connectionOptions);
+
+    return $conn;
+}	
+
+
+
+
+function getRoles(){
+    $conn = Conexion();
+    $query = 'SELECT * FROM tgenero';
+
+    $result = mysqli_query($conn,$query);
+    $listas = '<option value="0"> Seleccione Genero</option>';
+
+    while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+        $listas .= "<option value= '$row[id]'> $row[genero]</option>";
+    }
+    return $listas;
+}
+
+echo getRoles();
+
+?>
