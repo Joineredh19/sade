3ab74a2aa61e5741940bb08098079492e4bd48c9
antonyvale1 <antonio.vflores@alumnos.udg.mx>
2022-06-28 10:01:34 -0500

    Entrar

--- Asistencia/Asistencia.php
+++ Asistencia/Asistencia.php
@@ -1,24 +1,21 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Registro de entrada de alumnos</title>
-	<link rel="stylesheet" type="text/css" href="css/estilo_registro.css">
-</head>
-<body>
-
-	<?php 
+<?php
+		function Conexion(){
+			 $conn = mysqli_connect("localhost",
+			  "root", "Antony1201", "sade22");
+		 
+			 return $conn;
+		 }
+		 $conn = Conexion();
+		 
 		if (isset($_POST["enviar"])) {
-			$Codigoasis=$_POST["Codigoasis"];
-			$Codigosalida=$_POST["Codigoasis"];
+			$Codigoentrada=$_POST["email_tel"];
+			$Codigosalida=$_POST["email_tel"];
+			$Observacion=$_POST["observaciones"];
 
-			require_once("Conexion2.php");
+			require_once("../../Config/Conexion.config.php");
 
 			/*------------Validar datos de alumno------------*/
-			$query_alumno = mysqli_query($conexion, "SELECT Codigo FROM alumnos WHERE Codigo = '".$Codigoasis."'");
+			$query_alumno = mysqli_query($conn, "SELECT Codigo FROM tdatosalumnos WHERE Codigo = '".$Codigoentrada."'");
 				$result_alumno = mysqli_num_rows($query_alumno);
 				$data_alumno = mysqli_fetch_assoc($query_alumno);
 			/*-----------------------------------------------*/
@@ -31,86 +28,99 @@
 				$fecha_hoy = date('Y-m-d');
 
 				/*------------Validar datos de entrada------------*/
-				$query_entrada = mysqli_query($conexion, "SELECT FH_entrada, Codigoasis FROM alumnos_entrada 
-																	WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+				$query_entrada = mysqli_query($conn, "SELECT FHoraEntrada, Codigoentrada FROM tbitacorasalumnos 
+																	WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 						$result_entrada = mysqli_num_rows($query_entrada);
 						$data_entrada = mysqli_fetch_assoc($query_entrada);
-						if (isset($data_entrada['FH_entrada'])) {
-							$F_registro_entrada = date('Y-m-d', strtotime($data_entrada['FH_entrada']));
+						if (isset($data_entrada['FHoraEntrada'])) {
+							$F_registro_entrada = date('Y-m-d', strtotime($data_entrada['FHoraEntrada']));
 						}else{
 							$F_registro_entrada = date('1970-01-01');
 						}
 
-				$query_entrada2 = mysqli_query($conexion, "SELECT FH_entrada, Codigoasis FROM alumnos_entrada WHERE Codigoasis = '$Codigoasis'");
+				$query_entrada2 = mysqli_query($conn, "SELECT FHoraEntrada, Codigoentrada FROM tbitacorasalumnos WHERE Codigoentrada = '$Codigoentrada'");
 						$result_entrada2 = mysqli_num_rows($query_entrada2);
-				$query_entrada3 = mysqli_query($conexion, "SELECT FH_entrada, Codigoasis FROM alumnos_entrada 
-																	WHERE DATE(FH_entrada) = DATE(NOW()) AND Codigoasis = '$Codigoasis'");
+				$query_entrada3 = mysqli_query($conn, "SELECT FHoraEntrada, Codigoentrada FROM tbitacorasalumnos 
+																	WHERE DATE(FHoraEntrada) = DATE(NOW()) AND Codigoentrada = '$Codigoentrada'");
 						$result_entrada3 = mysqli_num_rows($query_entrada3);
 				/*-----------------------------------------------*/
 
 				/*------------Validar datos de salida------------*/
-				$query_salida = mysqli_query($conexion, "SELECT FH_salida, Codigosalida FROM alumnos_salida 
-																	WHERE Codigosalida = '$Codigosalida' ORDER BY FH_salida DESC LIMIT 1");
+				$query_salida = mysqli_query($conn, "SELECT FHoraSalida, Codigosalida FROM tbitacorasalumnos_salida 
+																	WHERE Codigosalida = '$Codigosalida' ORDER BY FHoraSalida DESC LIMIT 1");
 						$result_salida = mysqli_num_rows($query_salida);
 						$data_salida = mysqli_fetch_assoc($query_salida);
-						if (isset($data_salida['FH_salida'])) {
-							$F_registro_salida = date('Y-m-d', strtotime($data_salida['FH_salida']));
+						if (isset($data_salida['FHoraSalida'])) {
+							$F_registro_salida = date('Y-m-d', strtotime($data_salida['FHoraSalida']));
 						}else{
 							$F_registro_salida = date('1970-01-01');
 						}
 						
-				$query_salida2 = mysqli_query($conexion, "SELECT FH_salida, Codigosalida FROM alumnos_salida WHERE Codigosalida = '$Codigoasis'");
+				$query_salida2 = mysqli_query($conn, "SELECT FHoraSalida, Codigosalida FROM tbitacorasalumnos_salida WHERE Codigosalida = '$Codigosalida'");
 						$result_salida2 = mysqli_num_rows($query_salida2);
-				$query_salida3 = mysqli_query($conexion, "SELECT FH_salida, Codigosalida FROM alumnos_salida 
-																	WHERE DATE(FH_salida) = DATE(NOW()) AND Codigosalida = '$Codigoasis'");
+				$query_salida3 = mysqli_query($conn, "SELECT FHoraSalida, Codigosalida FROM tbitacorasalumnos_salida 
+																	WHERE DATE(FHoraSalida) = DATE(NOW()) AND Codigosalida = '$Codigosalida'");
 						$result_salida3 = mysqli_num_rows($query_salida3);
 				/*-----------------------------------------------*/
 				
 
-				if (empty($data_entrada['FH_entrada']) && empty($data_salida['FH_salida']) ) {
-					$insertar="INSERT INTO `alumnos_entrada`(`id`, `Codigoasis`, `FH_entrada`) VALUES 
+				if (empty($data_entrada['FHoraEntrada']) && empty($data_salida['FHoraSalida']) ) {
+					
+					$insertar="INSERT INTO `tbitacorasalumnos`(`id`, `Codigoentrada`, `FHoraEntrada`, `Observacion`) VALUES 
 							( NULL,
-							 '$Codigoasis',
-							 DEFAULT
+							 '$Codigoentrada',
+							 DEFAULT,
+							 '$Observacion'
 							)";
-							$query=mysqli_query($conexion, $insertar);
+							$query=mysqli_query($conn, $insertar);
 
-							$query=mysqli_query($conexion, "SELECT alumnos.Nombre, alumnos.Codigo, alumnos_entrada.FH_entrada  FROM alumnos_entrada INNER JOIN alumnos ON alumnos.Codigo = alumnos_entrada.Codigoasis WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+							$query=mysqli_query($conn, "SELECT tusuarios.Nombres, tdatosalumnos.Codigo, tbitacorasalumnos.FHoraEntrada FROM tbitacorasalumnos 
+																		INNER JOIN tdatosalumnos ON tdatosalumnos.Codigo = tbitacorasalumnos.Codigoentrada 
+																		INNER JOIN tusuarios ON tusuarios.id = tdatosalumnos.TUsuarios_id 
+																		WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 							$dato_registro = mysqli_fetch_assoc($query);
 
-							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombre'].")</h2>";
+							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombres'].")</h2>";
 
 				}else if ($fecha_hoy == $F_registro_entrada && $fecha_hoy == $F_registro_salida OR $fecha_hoy == $F_registro_entrada) {
 						
 						if ($result_entrada3 == $result_salida3) {
 	
-							$insertar="INSERT INTO `alumnos_entrada`(`id`, `Codigoasis`, `FH_entrada`) VALUES 
+							$insertar="INSERT INTO `tbitacorasalumnos`(`id`, `Codigoentrada`, `FHoraEntrada`, `Observacion`) VALUES 
 							( NULL,
-							 '$Codigoasis',
-							 DEFAULT
+							 '$Codigoentrada',
+							 DEFAULT,
+							 '$Observacion'
 							)";
-							$query=mysqli_query($conexion, $insertar);
-							
-							$query=mysqli_query($conexion, "SELECT alumnos.Nombre, alumnos.Codigo, alumnos_entrada.FH_entrada  FROM alumnos_entrada INNER JOIN alumnos ON alumnos.Codigo = alumnos_entrada.Codigoasis WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+							$query=mysqli_query($conn, $insertar);
+
+							$query=mysqli_query($conn, "SELECT tusuarios.Nombres, tdatosalumnos.Codigo, tbitacorasalumnos.FHoraEntrada FROM tbitacorasalumnos 
+																		INNER JOIN tdatosalumnos ON tdatosalumnos.Codigo = tbitacorasalumnos.Codigoentrada 
+																		INNER JOIN tusuarios ON tusuarios.id = tdatosalumnos.TUsuarios_id 
+																		WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 							$dato_registro = mysqli_fetch_assoc($query);
 
-							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombre'].")</h2>";
+							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombres'].")</h2>";
 	
 						}else{
 		
 							if($result_entrada3 > $result_salida3){
 		
-								$insertar2="INSERT INTO `alumnos_salida`(`id`, `Codigosalida`, `FH_salida`) VALUES 
+								$insertar2="INSERT INTO `tbitacorasalumnos_salida`(`id`, `Codigosalida`, `FHoraSalida`, `Observacion`) VALUES 
 								( NULL,
 								 '$Codigosalida',
-								 DEFAULT
+								 DEFAULT,
+								 '$Observacion'
 								)";
-								$query=mysqli_query($conexion, $insertar2);
+								$query=mysqli_query($conn, $insertar2);
 								
-								$query=mysqli_query($conexion, "SELECT alumnos.Nombre, alumnos.Codigo, alumnos_entrada.FH_entrada  FROM alumnos_entrada INNER JOIN alumnos ON alumnos.Codigo = alumnos_entrada.Codigoasis WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+								$query=mysqli_query($conn, "SELECT tusuarios.Nombres, tdatosalumnos.Codigo, tbitacorasalumnos.FHoraEntrada FROM tbitacorasalumnos 
+																		INNER JOIN tdatosalumnos ON tdatosalumnos.Codigo = tbitacorasalumnos.Codigoentrada 
+																		INNER JOIN tusuarios ON tusuarios.id = tdatosalumnos.TUsuarios_id 
+																		WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 								$dato_registro = mysqli_fetch_assoc($query);
 
-								echo "<h2 class='registrado'>Registrado con exito la asistencia de salida (". $dato_registro['Nombre'].")</h2>";
+								echo "<h2 class='registrado'>Registrado con exito la asistencia de salida (". $dato_registro['Nombres'].")</h2>";
 		
 							}
 		
@@ -119,31 +129,39 @@
 
 						if ($result_entrada2 == $result_salida2) {
 	
-							$insertar="INSERT INTO `alumnos_entrada`(`id`, `Codigoasis`, `FH_entrada`) VALUES 
+							$insertar="INSERT INTO `tbitacorasalumnos`(`id`, `Codigoentrada`, `FHoraEntrada`, `Observacion`) VALUES 
 							( NULL,
-							 '$Codigoasis',
-							 DEFAULT
+							 '$Codigoentrada',
+							 DEFAULT,
+							 '$Observacion'
 							)";
-							$query=mysqli_query($conexion, $insertar);
-							
-							$query=mysqli_query($conexion, "SELECT alumnos.Nombre, alumnos.Codigo, alumnos_entrada.FH_entrada  FROM alumnos_entrada INNER JOIN alumnos ON alumnos.Codigo = alumnos_entrada.Codigoasis WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+							$query=mysqli_query($conn, $insertar);
+
+							$query=mysqli_query($conn, "SELECT tusuarios.Nombres, tdatosalumnos.Codigo, tbitacorasalumnos.FHoraEntrada FROM tbitacorasalumnos 
+																		INNER JOIN tdatosalumnos ON tdatosalumnos.Codigo = tbitacorasalumnos.Codigoentrada 
+																		INNER JOIN tusuarios ON tusuarios.id = tdatosalumnos.TUsuarios_id 
+																		WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 							$dato_registro = mysqli_fetch_assoc($query);
 
-							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombre'].")</h2>";
+							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombres'].")</h2>";
 		
 						}else if ($result_entrada2 > $result_salida2) {
 	
-							$insertar="INSERT INTO `alumnos_entrada`(`id`, `Codigoasis`, `FH_entrada`) VALUES 
+							$insertar="INSERT INTO `tbitacorasalumnos`(`id`, `Codigoentrada`, `FHoraEntrada`, `Observacion`) VALUES 
 							( NULL,
-							 '$Codigoasis',
-							 DEFAULT
+							 '$Codigoentrada',
+							 DEFAULT,
+							 '$Observacion'
 							)";
-							$query=mysqli_query($conexion, $insertar);
+							$query=mysqli_query($conn, $insertar);
 
-							$query=mysqli_query($conexion, "SELECT alumnos.Nombre, alumnos.Codigo, alumnos_entrada.FH_entrada  FROM alumnos_entrada INNER JOIN alumnos ON alumnos.Codigo = alumnos_entrada.Codigoasis WHERE Codigoasis = '$Codigoasis' ORDER BY FH_entrada DESC LIMIT 1");
+							$query=mysqli_query($conn, "SELECT tusuarios.Nombres, tdatosalumnos.Codigo, tbitacorasalumnos.FHoraEntrada FROM tbitacorasalumnos 
+																		INNER JOIN tdatosalumnos ON tdatosalumnos.Codigo = tbitacorasalumnos.Codigoentrada 
+																		INNER JOIN tusuarios ON tusuarios.id = tdatosalumnos.TUsuarios_id 
+																		WHERE Codigoentrada = '$Codigoentrada' ORDER BY FHoraEntrada DESC LIMIT 1");
 							$dato_registro = mysqli_fetch_assoc($query);
-		
-							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombre'].")</h2>";
+
+							echo "<h2 class='registrado'>Registrado con exito la asistencia de entrada (". $dato_registro['Nombres'].")</h2>";
 		
 						}
 
@@ -152,22 +170,3 @@
 			}
 	}
 	?>
-
-	<form action="#" method="POST">
-		<fieldset>
-				<table>
-				<tr>
-					<h3 id="Titulo">Registre su asistencia</h3>		
-				</tr>
-				<tr>
-					<td><label>Codigo:</label></td>
-					<td><input type="number" required placeholder="Ingrese su codigo de alumno" id="Codigoasis" name="Codigoasis"></td>
-				</tr>			
-				<tr>
-					<td colspan="2" id="boton_registrar"><input type="submit" value="Registrar" name="enviar"></td>
-				</tr>
-			</table>
-		</fieldset>
-    </form>
-</body>
-</html>
