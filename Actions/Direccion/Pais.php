2f55a42771f071d2939cf4cf91cc80b7ace646be
Joiner2003 <Joineredh@gmail.com>
2022-07-01 10:58:35 -0500

    Actualizacion bd

--- Actions/Direccion/Pais.php
+++ Actions/Direccion/Pais.php
@@ -4,7 +4,7 @@ function Conexion(){
    // $serverName = "127.0.0.1";
   //  $connectionOptions = array("Database"=>"sade22","Uid"=>"root", "PWD"=>"", "CharacterSet"=>"UTF-8");
     $conn = mysqli_connect("cuc-udg22.mysql.database.azure.com",
-     "SADE22@cuc-udg22", "Sade_2022", "Sade22");
+     "SADE22@cuc-udg22", "Sade_2022", "bzya6emoafjbqjkolhiy");
    //  mysqli_connect($serverName, $connectionOptions);
 
     return $conn;
