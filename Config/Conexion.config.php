6745e70ca9862c20ca305658043e89552f903f87
Joiner2003 <Joineredh@gmail.com>
2022-07-01 09:56:52 -0500

    Conexion actualizada

--- Config/Conexion.config.php
+++ Config/Conexion.config.php
@@ -8,12 +8,6 @@ define('DB_USERNAME', 'ud5ggwh0iuxsr8up');
 define('DB_PASSWORD', 'SttfR9Di2pih75lEFuum');
 */
 
-   // $serverName = "127.0.0.1";
-  //  $connectionOptions = array("Database"=>"sade22","Uid"=>"root", "PWD"=>"", "CharacterSet"=>"UTF-8");
-  $conn = mysqli_connect("cuc-udg22.mysql.database.azure.com",
-  "SADE22@cuc-udg22", "Sade_2022", "Sade22");
-//  mysqli_connect($serverName, $connectionOptions);
-
 define('HOST', 'cuc-udg22.mysql.database.azure.com');
  define('DBNAME', 'Sade22');
  define('PORT', '3306');
