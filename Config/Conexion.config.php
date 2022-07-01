2bbc666186596ccc2d233b3d9c9b6eb29f2c867c
Joiner2003 <Joineredh@gmail.com>
2022-07-01 10:58:44 -0500

    Actualizacion BD

--- Config/Conexion.config.php
+++ Config/Conexion.config.php
@@ -9,7 +9,7 @@ define('DB_PASSWORD', 'SttfR9Di2pih75lEFuum');
 */
 
 define('HOST', 'cuc-udg22.mysql.database.azure.com');
- define('DBNAME', 'Sade22');
+ define('DBNAME', 'bzya6emoafjbqjkolhiy');
  define('PORT', '3306');
  define('USERNAME', 'SADE22@cuc-udg22');
  define('PASSWORD', 'Sade_2022');
