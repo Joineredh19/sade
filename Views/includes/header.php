a492fb2675b71424038b5c676dfafb5c3c3922ea
FranciscoBelloMarina <franciscojbmarina@gmail.com>
2022-07-06 15:31:28 -0500

    Graficassemanalesysemestrales

--- Views/includes/header.php
+++ Views/includes/header.php
@@ -37,6 +37,7 @@ $_SESSION['LAST_ACTIVITY'] = time();
   <link href="../Resource/css/demo/demo.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js"></script>
 </head>
 
 <div class="modal fade" id="registrar" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
