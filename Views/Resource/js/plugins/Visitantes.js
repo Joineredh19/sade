f1321ea436c50e1cf97cf5aca2a89a2f11b377f4
Adbel79 <adbel79@gmail.com>
2022-07-06 16:29:12 -0500

    select funcional de visitante

--- Views/Resource/js/plugins/Visitantes.js
+++ Views/Resource/js/plugins/Visitantes.js
@@ -12,3 +12,18 @@ $(document).ready(function(){
         alert('Error')
     })
 });
+
+$(document).ready(function(){
+
+    $.ajax({
+        type: 'POST',
+        url: '../../Actions/Visitantes/Visitantes.php',
+        data: {'peticion': 'Visitantes'}    
+    })
+    .done(function(listar_visitantes){
+        $('#ATrabajo').html(listar_visitantes)
+    })
+    .fail(function(){
+        alert('Error')
+    })
+});
--- Views/includes/footer.php
+++ Views/includes/footer.php
@@ -166,3 +166,20 @@ $(document).ready(function(){
   });
 });
 </script>
+
+<script>
+$(document).ready(function(){
+
+$.ajax({
+    type: 'POST',
+    url: '../../Actions/Visitantes/Visitantes.php',
+    data: {'peticion': 'Visitantes'}    
+})
+.done(function(listar_visitantes){
+    $('#ATrabajo').html(listar_visitantes)
+})
+.fail(function(){
+    alert('Error')
+})
+});
+</script>
