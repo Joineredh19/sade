d5f85338527a488709c65f523dc176c7aa114e00
minombre <leonardoanavarro@ms.uas.edu.mx>
2022-07-06 11:08:59 -0500

    vista admin a alumnos

--- Views/pages/alumnos.php
+++ Views/pages/alumnos.php
@@ -112,6 +112,24 @@ $msg = new \Plasticbrain\FlashMessages\FlashMessages();
                     </thead>
                     <tbody>
                     <tbody>
+                              <?php switch($HomeController->VerUsuarioxId($_SESSION['id'])->__GET('TRoles_id')){
+                                case 1:?>
+                                <?php foreach ($HomeController->ListarUsuario() as $key){ ?>
+                                  <?php if ($key->__GET('rol')=="Estudiante"){?>
+                                    <tr>
+                                        <td align="center"><?= $key->__GET('id') ?></td>
+                                        <td align="center"><?= $key->__GET('Nombres') ?></td>
+                                        <td  align="center"><?= $key->__GET('Apellidos') ?></td>
+                                        <td  align="center"><?= $key->__GET('Email') ?></td>
+                                        <td  align="center"><?= $key->__GET('Telefono') ?></td>
+                                        <td  align="center"><?= $key->__GET('genero') ?></td>
+                                        <td  align="center"><?= $key->__GET('rol') ?></td>
+                                        <td align="center" style="padding: 0;"> <a href="completarinfo.php?IdUsuario=<?= $key->__GET('id') ?>" target="_blank" title="Completar Información" class="btn btn-success"><i class="fa fa-pencil"></i></a></td>
+                                    <tr>
+                                  <?php };?>
+                                <?php };?>
+                              <?php break;
+                               case 4:?>
                                 <tr>
                                     <td align="center"><?= $HomeController->VerUsuarioxId($_SESSION['id'])->__GET('id') ?></td>
                                     <td align="center"><?= $HomeController->VerUsuarioxId($_SESSION['id'])->__GET('Nombres') ?></td>
@@ -123,7 +141,8 @@ $msg = new \Plasticbrain\FlashMessages\FlashMessages();
                                     <td align="center" style="padding: 0;"> <a href="completarinfo.php?IdUsuario=<?= $key->__GET('id') ?>" target="_blank" title="Alumnos" class="btn btn-success"><i class="fa fa-eye"></i></a></td>
                                   
                                   </tr>
-                          <?php #} ?>
+                            <?php break;?>
+                            <?php } ?>
                     </tbody>
                     </tbody>
                   </table>
