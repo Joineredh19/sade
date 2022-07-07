103db8c69e31e7e10fc11b9725751e35839b955d
Joiner2003 <Joineredh@gmail.com>
2022-07-07 15:38:01 -0500

    Modelo para guardado tutores

--- Models/TutorPadreModel.php
+++ Models/TutorPadreModel.php
@@ -1,19 +1,41 @@
-<?php 
-class TutorPadreModel
-{
-    private $id;
-    private $CalleyNum;
-    private $TUsuarios_id;
-    private $TColonias_id;
+<?php
 
-    public function __GET($k)
-    {
-        return $this->$k;
-    }
+require_once('../../Models/Conexion/Conexion.php');
+	/**
+	* 
+	*/
+	class TutorPadreModel
+	{
+		private $pdo;
 
-    public function __SET($k,$v)
-    {
-        return $this->$k = $v;
+		public function __CONSTRUCT()
+		{
+			try {
+				$this->pdo = Conexion::Conectar();
+			} catch(Exception $e) {
+				die($e->getMessage());
+			}
+		}
+		
+
+        public function CompletarInfo($Id, $Codigo,$NSeguroSocial,$Rfc,$Curp,$TipoSangre,$Alergia,$Selecolonia,$Calleynum,$Seletutoraca,$Selepadre,$Seleturno)
+		{
+			try {
+				$sql = ("INSERT INTO tdatosalumnos (Codigo, CalleyNum, NSeguroSocial, RFC, Curp,TipoSangre, Alergia,TTutoresAcademicos_id, TTutoresPadres_id,TColonias_id,TTurnos_id,TCalendariosIngresos_id,TUsuarios_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)");
+				$stm = $this->pdo->prepare($sql)->execute(array($Codigo,$Calleynum,$NSeguroSocial,$Rfc,$Curp,$TipoSangre,$Alergia,$Seletutoraca,$Selepadre,$Selecolonia,$Seleturno,1,$Id));
+				if($stm){
+					return true;
+				} else {
+					return false;
+				}
+			} catch (Exception $e) {
+				die($e->getMessage()." ->AlumnoModel->CompletarInfo()");
+			}
+		}
+
+
+
+
+
     }
-}
-?>
+	?>
