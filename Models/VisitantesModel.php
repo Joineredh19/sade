6e103c24b0e3d9b984d9404e34299ee03b3ee772
Adbel79 <adbel79@gmail.com>
2022-07-07 10:19:00 -0500

    modelos bitacoras

---
+++ Models/VisitantesModel.php
@@ -0,0 +1,21 @@
+<?php 
+class VisitantesModel
+{
+    private $id;
+    private $FhoraEntrada;
+    private $FhoraSalida;
+    private $ObservacionEntrada;
+    private $ObservacionSalida;
+    private $Codigo;
+
+    public function __GET($k)
+    {
+        return $this->$k;
+    }
+
+    public function __SET($k,$v)
+    {
+        return $this->$k = $v;
+    }
+}
+?>
