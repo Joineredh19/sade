04df72da6b803b7747d9748e4a01342287fb881a
Joiner2003 <Joineredh@gmail.com>
2022-07-07 10:53:24 -0500

    Controlador Listo para guardar

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -16,6 +16,7 @@ spl_autoload_register(function($nombreClase)
 		public function __CONSTRUCT() {
 		}
 
+		//Informacion de Usuario en General
 		public function AgregarUsuario($RolesId,$Nombres,$Apellidos,$Email,$Telefono,$Status,$Contrasenia,$generoId,$Fnto)
 		{
 			$model = new UsuarioModel();
@@ -46,9 +47,41 @@ spl_autoload_register(function($nombreClase)
 			return $var;
 		}
 
+		//Informacion De Rol de Alumno
+
 		public function vistaalumnos(){
 			$model = new UsuarioModel;
 			$var = $model->vistaalumnos();
 			return $var;
 		}
+
+		public function CompletarAlumnos($Id, $Codigo,$NSeguroSocial,$Rfc,$Curp,$TipoSangre,$Alergia,$Selecolonia,$Calleynum,$Seletutoraca,$Selepadre,$Seleturno)
+		{
+			$model = new AlumnoModel();
+			$var = $model->CompletarInfo($Id, $Codigo,$NSeguroSocial,$Rfc,$Curp,$TipoSangre,$Alergia,$Selecolonia,$Calleynum,$Seletutoraca,$Selepadre,$Seleturno);
+			return $var;
+		}
+
+
+		//Informacion de Rol de Tutor Academico
+		public function CompletarTutorAca($Id,$Selecolonia,$Calleynum)
+		{
+			$model = new TrabajadorModel();
+			$var = $model->CompletarInfo($Id,$Selecolonia,$Calleynum);
+			return $var;
+		}
+
+
+
+		//Informacion de Rol Tutor Padre
+
+		public function CompletarTutorPadre($Id,$Selecolonia,$Calleynum)
+		{
+			$model = new TutorPadreModel();
+			$var = $model->CompletarInfo($Id,$Selecolonia,$Calleynum);
+			return $var;
+		}
+
+
+
 	}
