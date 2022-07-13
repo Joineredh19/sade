7ab688fefdb72df71adc75d26902c838cfab1910
leonelgh22 <leonelgahn@gmail.com>
2022-07-13 16:57:41 -0500

    prueba2

--- Views/pages/perfil.php
+++ Views/pages/perfil.php
@@ -53,7 +53,7 @@ $msg = new \Plasticbrain\FlashMessages\FlashMessages();
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="javascript:;">Perfill </a>
+            <a class="navbar-brand" href="javascript:;">Perfil </a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
