97c649f8e31b5c31b098729fd96ab357c09e7c79
Joiner2003 <Joineredh@gmail.com>
2022-07-14 15:17:32 -0500

    cambio de conexion

--- Config/Conexion.config.php
+++ Config/Conexion.config.php
@@ -7,17 +7,17 @@ define('DB_PORT', '3306');
 define('DB_USERNAME', 'ud5ggwh0iuxsr8up');
 define('DB_PASSWORD', 'SttfR9Di2pih75lEFuum');
 */
-
+/*
 define('HOST', 'softwarela.org');
 define('DBNAME', 'softwax6_sade2022');
 define('PORT', '3306');
 define('USERNAME', 'softwax6_sade22');
 define('PASSWORD', 'Sade2022*');
-
-/* define('HOST', 'cuc-udg22.mysql.database.azure.com');
+*/
+define('HOST', 'cuc-udg22.mysql.database.azure.com');
  define('DBNAME', 'bzya6emoafjbqjkolhiy');
  define('PORT', '3306');
  define('USERNAME', 'SADE22@cuc-udg22');
- define('PASSWORD', 'Sade_2022');*/
+ define('PASSWORD', 'Sade_2022');
 
 ?>
