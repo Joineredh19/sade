fdeb7136e91a475bbc6421bfd98e3cbb1eed69bc
Joiner2003 <Joineredh@gmail.com>
2022-07-18 13:02:00 -0500

    Guardado

--- Actions/CorteAsistencia/CortarAsistencia.php
+++ Actions/CorteAsistencia/CortarAsistencia.php
@@ -22,37 +22,53 @@ function Conexion(){
 		FROM tusuarios INNER JOIN tdatosalumnos ON tusuarios.id = tdatosalumnos.TUsuarios_id INNER JOIN tturnos ON tdatosalumnos.TTurnos_id = tturnos.id 
 		WHERE tdatosalumnos.TTurnos_id = 1');
 		$listas= array();
-		while ($row = mysqli_fetch_assoc($result)) {
+
+
+		while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+			printf (" Codigo: %s", $row["codigo"]);
 			//echo($row["codigo"]);
-		$codigos = explode(',', $row['codigo']);
-		$listas[$row['codigo']] = $codigos;
+		$codigos = explode('codigo'. ' =>' .',', $row['codigo']);
+		$listas["Codigo"] = $codigos;
 		}
-		echo json_encode($listas);
+		
+		//echo json_encode($listas);
 	$result2 = mysqli_query($conn,'SELECT codigo FROM tbitacorasalumnos');
 		$listasBitacoras= array();
-		while ($row = mysqli_fetch_assoc($result2)) {
-			$codigos2 = explode(',', $row['codigo']);
-			$listasBitacoras["prueba"['codigo']] = $codigos2;
+		while ($row = mysqli_fetch_array($result2,MYSQLI_ASSOC)) {
+			$codigos2 = explode('codigo'. ' =>' .',', $row['codigo']);
+			$listasBitacoras["Codigo"] = $codigos2;
 		}
+		//echo json_encode($listasBitacoras);
 
-		$resultado = array_diff_uassoc($listas, $listasBitacoras);
+		$resultado = array_diff_assoc($listas, $listasBitacoras);
+
+		$userObj = (object) $resultado;
+		$json =  json_encode($userObj);
+		echo $json;
+		//print_r(json_decode($json));
+		//$jsone = (json_decode($json));
+
 
 		if($resultado==0){
 			echo("no hay registros");
 		}else{
-			print_r($resultado);
-			$array_Insertar = json_encode($resultado);
-			foreach ($array_Insertar as $row) {
-				$codigo = $row['1'];
+		//	print_r($resultado);
+		//	$array_Insertar = ;
+		//	$array_Insertar = json_decode(json_encode($resultado));
+
+			//echo json_decode($array_Insertar);
+			foreach ($userObj as $row) {
+				$codigo = $row[0];
+				//var_dump($row[0]);
 				$sql = "INSERT INTO tinasistencias (codigo) VALUES ('$codigo')";
         		$conn->query($sql);
 			}
-			for ($i = 0; $i< count($resultado); $i++ ){
+		/*	for ($i = 0; $i< count($resultado); $i++ ){
 				$codigoInsertar = $array_Insertar[count($resultado)];
 				echo($array_Insertar);
 				$sql = "INSERT INTO tinasistencias (codigo) VALUES ('$codigoInsertar')";
         		$conn->query($sql);
-			}
+			}*/
 			if ($conn->query($sql) === TRUE) {
 				echo "se ha guardado correctamente";
 			} else {
@@ -62,9 +78,7 @@ function Conexion(){
 		}
 		//var_dump($listasBitacoras);
 
-	}
-}
-
+	}}
 
 
 		/*
