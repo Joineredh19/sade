b1188fbed5c55b8c8823fb1b809649ee311057b8
iAmBeeGees <walk414126@gmail.com>
2022-07-28 18:40:39 -0500

    Formulario Inasistencias

---
+++ Views/pages/controlasistencias.php
@@ -0,0 +1,105 @@
+<?php include '../includes/header.php';
+?>
+
+<?php
+require_once('../../Controllers/HomeController.php');
+$HomeController = new HomeController();
+
+require '../../Lib/FlashMessages.php';
+if (!session_id()) @session_start();
+// Instantiate the class
+$msg = new \Plasticbrain\FlashMessages\FlashMessages();
+date_default_timezone_set('America/Mexico_City');
+
+if (isset($_GET['IdUsuario'])) {
+  $id = $_GET['IdUsuario'];
+  $data = $HomeController->VerUsuarioxId($id);
+}else{
+  $data = NULL;
+}
+
+
+?>
+ <link rel="apple-touch-icon" sizes="76x76" href="../Resource/img/UDG- VectorR.png">
+ <link rel="icon" type="image/png" href="../Resource/img/SLALogo.png">
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="white" data-active-color="danger">
+      <div class="logo">
+        <a href="http://www.cuc.udg.mx/" class="simple-text logo-mini">
+          <div class="logo-image-small">
+            <img src="../Resource/img/SLALogo.png">
+          </div>
+          <!-- <p>CT</p> -->
+        </a>
+        <a href="http://www.cuc.udg.mx/" class="simple-text logo-normal">
+        SADE
+          <!-- <div class="logo-image-big">
+            <img src="../assets/img/logo-big.png">
+          </div> -->
+        </a>
+      </div>
+      <div class="sidebar-wrapper">
+        <ul class="nav">
+        <?php 
+      include '../includes/sidebar.php';
+      ?>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+      <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="javascript:;">Completar Información</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navigation">
+            <form>
+              <div class="input-group no-border">
+                <input type="text" value="" class="form-control" placeholder="Buscar...">
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <i class="nc-icon nc-zoom-split"></i>
+                  </div>
+                </div>
+              </div>
+            </form>
+      <?php 
+      include '../includes/navbar.php';
+      ?>
+      </nav>
+      <!-- End Navbar -->
+
+      <div class="content">
+        <div class="row">
+       <!-- Login Form -->
+     <form method="post"  action="<?php echo '../../Actions/register/actualizar.php';?>"  enctype="multipart/form-data">
+   
+  
+  <input type="text"  class="form-control" id="descripcionfalta"  name="curp1" placeholder="Descripción de falta">
+  <br>
+  <select name="Aptitud" id="Aprovechamiento" class="form-control " aria-label="Aptitud"> </select>
+  <br>
+  <select name="Aprovechamiento" id="Aprovechamiento" class="form-control " aria-label="Aprovechamiento"> </select>
+ 
+ 
+    <button type="submit" class="btn btn-success " id="btnRegistrarse" value=""> Enviar</button>
+    </form>
+        </div>
+   
+
+      <?php include '../includes/footer.php';?>
+   
