dbbe93cdbd712e63f72ce0301c6eeec752f20682
antonyvale1 <antonio.vflores@alumnos.udg.mx>
2022-08-03 01:09:51 -0500

    Enter

--- Views/Resource/charts/chartbar.php
+++ Views/Resource/charts/chartbar.php
@@ -5,7 +5,8 @@
     </head>
 <!-- INICIO TABLA SEMANAL -->
 <div>
-<?php 
+<?php
+include '../../Asistencia/GraficaInasistencia.php'; 
 $DIASSEMANA = array('LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES');
 $dias = json_encode($DIASSEMANA);
 ?>
@@ -18,20 +19,14 @@ $dias = json_encode($DIASSEMANA);
                         data: {
                               labels: <?php echo $dias;?>,
                               datasets: [{
-                                          label: 'FALTAS TOTALES',
-                                          data: [30, 44, 27, 24, 23],
-                                          backgroundColor: ['rgba(141, 153, 174)',],
-                                          borderColor:'rgb(43,45,66)',
-                                        },
-                                        {
                                           label: 'FALTAS ATENDIDAS',
-                                          data: [20, 26, 12, 16, 10],
+                                          data: [<?php echo $LunesAten.', '.$MartesAten.', '.$MiercolesAten.', '.$JuevesAten.', '.$ViernesAten?>],
                                           backgroundColor: ['rgba(27, 157, 51)',],
                                           borderColor:'rgb(27, 157, 51)',
                                         },
                                         {
                                           label: 'FALTAS SIN ATENDER',
-                                          data: [10, 18, 15, 8, 13],
+                                          data: [<?php echo $LunesIna.', '.$MartesIna.', '.$MiercolesIna.', '.$JuevesIna.', '.$ViernesIna?>],
                                           backgroundColor: ['rgba(207,4,41)',],
                                           borderColor:'rgb(207,4,41)',
                                         }]
